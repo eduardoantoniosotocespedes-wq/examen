7bfe702ac96af74626d3eb1b866dcae3c2e7f068
EduardoSoto <eduardoantoniosotocespedes@gmail.com>
2026-09-16 08:09:07 -0400

    segundo comentario

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,9 @@
-1
+#fibonacci
+a = 0
+b = 1
+n = int(input("ingrese un numero: "))
+for i in range(0, n+1):
+    print(a)
+    c = a + b
+    a = b
+    b = c
--- ejercicio3.py
+++ ejercicio3.py
@@ -1,1 +1,10 @@
-3
+#Tabla de multiplicar
+print("===Tabla de ultiplicar===")
+while True:
+    x = int(input("Porfavor ingrese un numero entre el 1 y el 10: "))
+    if x <11 and x >0:
+        for i in range (1, 13):
+            print(f"{i} x {x} = {i*x}")
+        break
+    else:
+        print("Error intente denuevo...")
